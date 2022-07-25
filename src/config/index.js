b8ca02324576a09ed975a76531d1ae39c2ee51cf
prakash-thoughtclan <prakash@thoughtclan.com>
2022-07-25 10:27:25 +0530

    API change

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,8 @@
-// export const BASE_API_URI = 'https://doctor-management-api-prakashvpsm.vercel.app/api/v1/users'
-
-// export const BASE_API_URI_MAIN = 'https://doctor-management-api-prakashvpsm.vercel.app/api/v1'
+// export const BASE_API_URI_MAIN = 'http://localhost:3002/api/v1'
 
 
 
-export const BASE_API_URI_MAIN = 'http://localhost:3002/api/v1'
+export const BASE_API_URI_MAIN = 'https://doctor-management-api-prakashvpsm.vercel.app/api/v1'
 export const BASE_API_URI = `${BASE_API_URI_MAIN}/users`
 export const BASE_API_URI_SLOTS = `${BASE_API_URI_MAIN}/slots`
 export const BASE_API_URI_PATIENTS = `${BASE_API_URI_MAIN}/patients`
